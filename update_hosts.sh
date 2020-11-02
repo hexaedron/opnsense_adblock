0cb023a5486ed3ec5071c86b258988b661d1ff26
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2020-11-02 13:15:39 +0300

    Fixed a dnsmasq quirk with retracker.local

--- update_hosts.sh
+++ update_hosts.sh
@@ -3,3 +3,8 @@
 fetch -o - https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts | \
 grep -E '^0.0.0.0' | \
 awk '{print $1 " "  $2}' > /home/cube/adblock/hosts.add
+
+# OPNSense's dnsmasq works quirky without this line.
+# It's kinda legacy Bittorent setting, so removing it
+# is completely harmless nowadays
+echo "0.0.0.0 retracker.local" >> hosts.add
--- update_hosts_MVPS.sh
+++ update_hosts_MVPS.sh
@@ -4,3 +4,8 @@ fetch -o - http://winhelp2002.mvps.org/hosts.zip | \
 unzip -p - | \
 grep -E '^0.0.0.0' | \
 awk '{print $1 " "  $2}' > /home/cube/adblock/hosts.add
+
+# OPNSense's dnsmasq works quirky without this line.
+# It's kinda legacy Bittorent setting, so removing it
+# is completely harmless nowadays
+echo "0.0.0.0 retracker.local" >> hosts.add
